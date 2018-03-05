1cd07c2d42375b22963f856ed0c6b14a65f8da6f
Mark Mikelonis <mark.mikelonis@gmail.com>
2018-03-04 18:11:18 -0800

    updated schema

--- db/schema.sql
+++ db/schema.sql
@@ -7,7 +7,7 @@ CREATE TABLE bands(
     band_name VARCHAR(50) NOT NULL,
     band_pic_url VARCHAR(200),
     bio VARCHAR(300), 
-    b_locale VARCHAR(200) NOT NULL,
+    b_locale INTEGER(10) NOT NULL,
     PRIMARY KEY(b_id)
 );
 
@@ -33,7 +33,7 @@ CREATE TABLE fans(
     fan_name VARCHAR(50) NOT NULL,
     email VARCHAR(50) NOT NULL,
     fan_pic_url VARCHAR(200),
-    f_locale VARCHAR(200) NOT NULL,
+    f_locale INTEGER(10) NOT NULL,
     b_id INTEGER(10) NOT NULL,
     PRIMARY KEY(f_id),
     FOREIGN KEY(b_id) REFERENCES bands(b_id)
